b7a3654c0299b934d0f0df92ebc1844456ae156b
Pablo Fonseca <pablofonseca777@gmail.com>
2019-05-29 19:49:57 -0300

    Fix verse_2 test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,7 +2,8 @@ class Bottles
   def verse(number)
     <<-VERSE
 #{number} bottles of beer on the wall, #{number} bottles of beer.
-Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+Take one down and pass it around, #{number - 1} #{number - 1 == 1 ? "bottle" :
+"bottles"} of beer on the wall.
     VERSE
   end
 end
