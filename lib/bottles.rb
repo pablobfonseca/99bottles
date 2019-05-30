85f3f0cdc84771bfc22745b980eb7b4c85ce585d
Pablo Fonseca <pablofonseca777@gmail.com>
2019-05-29 23:25:39 -0300

    Refactor: Use Struct as value objects to render sentences
    PS: This is just one way of solving, not the final solution 😅

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,3 +1,39 @@
+FirstSentence = Struct.new(:number) do
+  def render
+    return "No more bottles" if number == 0
+    bottles = number == 1 ? "bottle" : "bottles"
+    return "#{number} #{bottles}"
+  end
+end
+
+SecondSentence = Struct.new(:number) do
+  def render
+    return "no more bottles" if number == 0
+    bottles = number == 1 ? "bottle" : "bottles"
+    "#{number} #{bottles}"
+  end
+end
+
+ThirdSentence = Struct.new(:number) do
+  def render
+    return "Go to the store and buy some more" if number == 0
+    it_or_one = number == 1 ? "it" : "one"
+    "Take #{it_or_one} down and pass it around"
+  end
+end
+
+LastSentence = Struct.new(:number) do
+  def render
+    minus = number - 1
+
+    return "99 bottles" if number == 0
+    return "no more bottles" if minus == 0
+    return "1 bottle" if number == 1 || minus == 1
+
+    "#{minus} bottles"
+  end
+end
+
 class Bottles
   def song
     verses(99, 0)
@@ -17,30 +53,18 @@ class Bottles
   private
 
   def first_sentence(number)
-    return "No more bottles" if number == 0
-    bottles = number == 1 ? "bottle" : "bottles"
-    return "#{number} #{bottles}"
+    FirstSentence.new(number).render
   end
 
   def second_sentence(number)
-    return "no more bottles" if number == 0
-    bottles = number == 1 ? "bottle" : "bottles"
-    "#{number} #{bottles}"
+    SecondSentence.new(number).render
   end
 
   def third_sentence(number)
-    return "Go to the store and buy some more" if number == 0
-    it_or_one = number == 1 ? "it" : "one"
-    "Take #{it_or_one} down and pass it around"
+    ThirdSentence.new(number).render
   end
 
   def last_sentence(number)
-    minus = number - 1
-
-    return "99 bottles" if number == 0
-    return "no more bottles" if minus == 0
-    return "1 bottle" if number == 1 || minus == 1
-
-    "#{minus} bottles"
+    LastSentence.new(number).render
   end
 end
