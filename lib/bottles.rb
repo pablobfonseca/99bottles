4f8cc58bbe2223a37f06e9dd6bf16c8ccc083594
Pablo Fonseca <pablofonseca777@gmail.com>
2019-05-29 22:54:57 -0300

    Fix verse_1 test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,25 @@
 class Bottles
   def verse(number)
     <<-VERSE
-#{number} bottles of beer on the wall, #{number} bottles of beer.
-Take one down and pass it around, #{number - 1} #{number - 1 == 1 ? "bottle" :
-"bottles"} of beer on the wall.
+#{number} #{number == 1 ? "bottle" : "bottles"} of beer on the wall, #{handle_sentence(number)} of beer.
+Take #{number == 1 ? "it" : "one"} down and pass it around, #{handle_minus(number)} of beer on the wall.
     VERSE
+  end
+
+  private
+
+  def handle_sentence(number)
+    return "1 bottle" if number == 1
+
+    "#{number} bottles"
+  end
+
+  def handle_minus(number)
+    minus = number - 1
+
+    return "no more bottles" if minus == 0
+    return "1 bottle" if number == 1 || minus == 1
+
+    "#{minus} bottles"
   end
 end
